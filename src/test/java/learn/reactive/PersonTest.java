bf7e99bff949849e9d5621599400a4ba55fba553
Athul Rajeev <athul.rajeev@emc.com>
2016-06-25 18:23:42 +0530

    First commit

---
+++ src/test/java/learn/reactive/PersonTest.java
@@ -0,0 +1,15 @@
+package learn.reactive;
+
+import learn.reactive.Person;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void canConstructAPersonWithAName() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+    }
+}
